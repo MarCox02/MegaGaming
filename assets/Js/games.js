e7c915e85603f9e0bd5f3b6fdcd2cb388f86fa04
MarCox02 <Cox.Martin2002@gmail.com>
2024-05-30 12:08:44 -0400

    1.02.01

---
+++ assets/Js/games.js
@@ -0,0 +1,25 @@
+export const games = [
+    {
+        "id":"g01",
+        "nombre": "Helldivers 2",
+        "desarrolladores": "ArrowHead Studios",
+        "genero":"Co-op Shooter",
+        "descripcion":"Helldivers 2 es un juego de disparos multijugador online en el que te unes a un escuadrón con un máximo de tres amigos, así que no hay modos de juego offline o de un solo jugador.",
+    }
+];
+export const console = [
+    {
+        "id":"c01",
+        "nombre": "Xbox XS",
+        "creadores":"Microsoft",
+        "precio":350000,
+    }
+];
+export const accesories = [
+    {
+        "id":"a01",
+        "nombre": "Control Xbox Xs",
+        "consola":"Xbox",
+        "precio":40000,
+    }
+];
---
+++ assets/Js/index.js
@@ -0,0 +1,60 @@
+import {games, console, accesories} from './games.js' ;
+
+function getGames(){
+
+    let gamesRow = document.getElementById("gamesRow");
+    
+    games.map(function(games){
+
+        const divCol = document.createElement('div');
+        divCol.classList.add('col-xl-3');
+        divCol.classList.add('col-lg-3');
+        divCol.classList.add('col-md-3');
+        divCol.classList.add('col-sm-3');
+        divCol.classList.add('col-xs-3');
+        divCol.classList.add('mt-2');
+        divCol.classList.add('mb-2');
+    
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const img = document.createElement('img');
+        img.src = "https://placehold.co/600x400/EEE/31343C";
+        img.alt = `Imagen de ${games.nombre}`;
+        img.classList.add('card-img-top');
+
+        const divBody = document.createElement('div');
+        divBody.classList.add('card-body');
+
+        const title = document.createElement('h5');
+        title.classList.add('cart-title');
+        title.textContent = `${games.nombre}`;
+
+        const subtitle = document.createElement('p');
+        subtitle.classList.add('card-text');
+        subtitle.textContent = `Desarrollador: ${games.desarrolladores} `;
+
+        const subtitle2 = document.createElement('p');
+        subtitle2.classList.add('card-text');
+        subtitle2.textContent = `${games.descripcion} `;
+            
+
+        divBody.appendChild(title);
+        divBody.appendChild(subtitle);
+        divBody.appendChild(subtitle2);
+
+        card.appendChild(img);
+        card.appendChild(divBody);
+        
+
+        divCol.appendChild(card);
+
+        gamesRow.appendChild(divCol);
+
+
+
+
+
+    })
+}
+getGames()
