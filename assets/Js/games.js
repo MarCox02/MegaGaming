b96402d8be3e87dc91bd649dfffc9aa0f956136d
ha.quilodran <ha.quilodran@duocuc.cl>
2024-05-30 12:43:56 -0400

    aumento_de_variedad

--- assets/Js/games.js
+++ assets/Js/games.js
@@ -5,7 +5,41 @@ export const games = [
         "desarrolladores": "ArrowHead Studios",
         "genero":"Co-op Shooter",
         "descripcion":"Helldivers 2 es un juego de disparos multijugador online en el que te unes a un escuadrón con un máximo de tres amigos, así que no hay modos de juego offline o de un solo jugador.",
-    }
+        "precio": 40000,
+    },
+    {
+        "id":"g02",
+        "nombre": "Elden Ring",
+        "desarrolladores": "Fromsoftware",
+        "genero":"RPG Mundo abierto",
+        "descripcion":"Elden Ring es un juego de mundo abierto en el que explorar el mundo de las tierras intermedias, descubre una gran variedad de enemigos, jefes, armas y secretos en tu camino a convertirte en el Elden Lord.",
+        "precio": 60000,
+    },
+    {
+        "id":"g03",
+        "nombre": "Cyberpunk 2077",
+        "desarrolladores": "CD Projekt",
+        "genero":"Shooter RPG Mundo Abierto",
+        "descripcion":"Cyberpunk 2077 se situa en un futuro de tematica Cyberpunk, dentro de Night City, California, Donde tomas el papel de V, un mercenario que ha terminado con biochip ajeno implantado, que amenaza con terminar su vida.",
+        "precio": 60000,
+    },
+    {
+        "id":"g04",
+        "nombre": "Monster Hunter World",
+        "desarrolladores": "Capcom",
+        "genero":"Co-op RPG de accion",
+        "descripcion":"Participa en cacerias de increibles bestias que habitan el mundo de Monster Hunter World con hasta otros 3 cazadores, utliza tus recompensas para mejorar tu equipamiento y vuelve a cazar monstruos aun mas grandes.",
+        "precio": 30000,
+    },
+    {
+        "id":"g05",
+        "nombre": "Cyberpunk 2077",
+        "desarrolladores": "CD Projekt",
+        "genero":"Shooter RPG Mundo Abierto",
+        "descripcion":"Cyberpunk 2077 se situa en un futuro de tematica Cyberpunk, dentro de Night City, California, Donde tomas el papel de V, un mercenario que ha terminado con biochip ajeno implantado, que amenaza con terminar su vida.",
+        "precio": 60000,
+    },
+    
 ];
 export const console = [
     {
@@ -13,7 +47,19 @@ export const console = [
         "nombre": "Xbox XS",
         "creadores":"Microsoft",
         "precio":350000,
-    }
+    },
+    {
+        "id":"c02",
+        "nombre": "PlayStation 5",
+        "creadores":"Sony",
+        "precio":500000,
+    },
+    {
+        "id":"c03",
+        "nombre": "Nintendo Switch",
+        "creadores":"Nintendo",
+        "precio":300000,
+    },
 ];
 export const accesories = [
     {
@@ -21,5 +67,17 @@ export const accesories = [
         "nombre": "Control Xbox Xs",
         "consola":"Xbox",
         "precio":40000,
-    }
+    },
+    {
+        "id":"a02",
+        "nombre": "Control PlayStation 5",
+        "consola":"PlayStation 5",
+        "precio":40000,
+    },
+    {
+        "id":"a03",
+        "nombre": "Joycon Nintendo Switch",
+        "consola":"Nintendo Switch",
+        "precio":40000,
+    },
 ];
